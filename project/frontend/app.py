341ac7b7f800d58aceabc8fc5558776c8cc71594
Bryce Williams <bwilliams2@gmail.com>
2023-09-18 23:52:48 -0500

    Update app

--- project/frontend/app.py
+++ project/frontend/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+import urllib
 import requests
 from PIL import Image
 import numpy as np
@@ -42,17 +43,36 @@ def process_uploaded_file(uploaded_file):
     image = transform(image).float()
     image = torch.tensor(image)
     image = image.unsqueeze(0)
-    data = image.numpy()
-    input_data = json.dumps({"data": data.tolist()})
+    r_data = image.numpy()
+    r_data = json.dumps({"data": r_data.tolist()})
+
+    body = str.encode(r_data)
 
 
-    with tempfile.TemporaryDirectory() as tmpdir:
-        # use the temporary directory here
-        file = os.path.join(tmpdir, "request.json")
-        with open(file, "w") as outfile:
-            outfile.write(input_data)
-        
-        result = {"label": "ship", "probability": "0.9999"}
+    url = 'https://pytorch-model-endpoint.eastus.inference.ml.azure.com/score'
+    # Replace this with the primary/secondary key or AMLToken for the endpoint
+    api_key = os.getenv("ENDPOINT_KEY")
+    if not api_key:
+        raise Exception("A key should be provided to invoke the endpoint")
+
+    # The azureml-model-deployment header will force the request to go to a specific deployment.
+    # Remove this header to have the request observe the endpoint traffic rules
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'pytorch-deployment' }
+
+    req = urllib.request.Request(url, body, headers)
+
+    try:
+        response = urllib.request.urlopen(req)
+
+        result = json.loads(response.read().decode())
+        print(result)
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
+
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+        print(error.info())
+        print(error.read().decode("utf8", 'ignore'))
+
     return result
 
     
